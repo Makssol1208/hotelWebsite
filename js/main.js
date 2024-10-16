5c798a4287d5f213551486e14e950d8b2442ea39
Maksym Soloviov <maxim.sv143@gmail.com>
2024-10-16 17:17:22 +0300

    commit
    made Facilities page

--- js/main.js
+++ js/main.js
@@ -16,3 +16,13 @@ $('.carousel-btn--prev').click(function () {
     owl.trigger('prev.owl.carousel');
 });
 
+ScrollReveal({
+    reset: true,
+    distance: "100px",
+    duration: 2000,
+    delay: 100
+});
+
+ScrollReveal().reveal('.hero__title',{delay: 400, origin: "left"});
+ScrollReveal().reveal('.hero__main',{delay: 500, origin: "left"});
+ScrollReveal().reveal('.hero__bottom, .hero__text',{delay: 600, origin: "left"});
