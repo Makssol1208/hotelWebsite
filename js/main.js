f335f2338beeabb1206c74d869bd71e27846ce27
Maksym Soloviov <maxim.sv143@gmail.com>
2024-10-16 19:10:04 +0300

    commit
    added Modal Window

--- js/main.js
+++ js/main.js
@@ -23,6 +23,41 @@ ScrollReveal({
     delay: 100
 });
 
-ScrollReveal().reveal('.hero__title',{delay: 400, origin: "left"});
-ScrollReveal().reveal('.hero__main',{delay: 500, origin: "left"});
-ScrollReveal().reveal('.hero__bottom, .hero__text',{delay: 600, origin: "left"});
+ScrollReveal().reveal('.hero__title', { delay: 400, origin: "left" });
+ScrollReveal().reveal('.hero__main', { delay: 500, origin: "left" });
+ScrollReveal().reveal('.hero__bottom, .hero__text', { delay: 600, origin: "left" });
+
+// Modal
+const modalController = () => {
+    const modal = document.querySelector('.booking-modal');
+    const bookBtn = document.querySelector('.header-btn');
+
+    modal.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 300ms ease-in-out;
+`;
+
+    const closeModal = event => {
+        const target = event.target;
+
+        if (target === modal || target.closest('.modal__close')) {
+            modal.style.opacity = 0;
+        }
+
+        setTimeout(() => {
+            modal.style.visibility = 'hidden';
+        }, 300);
+    }
+
+    const modalOpen = () => {
+        modal.style.visibility = 'visible';
+        modal.style.opacity = 1;
+    }
+
+    bookBtn.addEventListener('click', modalOpen);
+    modal.addEventListener('click', closeModal);
+};
+
+modalController();
